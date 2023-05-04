963f0abd74de4226764a7b5bb7c9bebbc4be1d6e
dramnay <deepanshu.ramnay@gmail.com>
2023-05-04 18:10:10 +0530

    db connection

--- service/user_service.js
+++ service/user_service.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Product = require("../model/products");
 
-exports.createProduct = async (product) => {
-  try {
-    await product.save();
-    console.log("Product created successfully", product);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// exports.createProduct = async (product) => {
+//   try {
+//     await product.save();
+//     console.log("Product created successfully", product);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 exports.createUser = async (user) => {
   await user.save();
