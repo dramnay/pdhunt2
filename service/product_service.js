55915d6e97c5d24976f25b60680da16c8eec57c7
dramnay <deepanshu.ramnay@gmail.com>
2023-05-04 11:44:32 +0530

    route files added

---
+++ service/product_service.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+// const Product = require("../model/products");
+
+// exports.createProduct = async (product) => {
+//   try {
+//     await product.save();
+//     console.log("Product created successfully", product);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
