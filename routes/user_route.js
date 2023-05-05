13c3ad3e579d5a0205c272d21c95eeb03f1d6f61
dramnay <deepanshu.ramnay@gmail.com>
2023-05-05 11:04:35 +0530

    get user by email

--- routes/user_route.js
+++ routes/user_route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { createUser, getUserByEmail } = require("../controller/user_controller");
 
 router.route("/").post(createUser);
-// router.route("/").post(getUserByEmail);
+router.route("/email").post(getUserByEmail);
 module.exports = router;
