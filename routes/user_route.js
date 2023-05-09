0c69e1394826333b7905df56aae80777a288f8b6
dramnay <deepanshu.ramnay@gmail.com>
2023-05-09 16:58:34 +0530

    jwt token added

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createUser, getUserByEmail } = require("../controller/user_controller");
-
-router.route("/").post(createUser);
-router.route("/email").post(getUserByEmail);
+const { verifyToken } = require("../controller/auth_controller");
+// router.route("/").post(createUser);
+router.route("/").post(verifyToken, getUserByEmail);
 module.exports = router;
